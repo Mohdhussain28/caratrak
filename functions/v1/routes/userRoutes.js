f34b480f4a7df16ee90974619cacc922a02bf51d
Mohdhussain28 <mohdhussain28@outlook.com>
2024-04-09 16:10:10 +0530

    ..

--- functions/v1/routes/userRoutes.js
+++ functions/v1/routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { signup } = require("../controllers/user");
 
 const router = express.Router();
-router.route('/signup').post(signup);
+router.route("/signup").post(signup);
 
 module.exports = router;
