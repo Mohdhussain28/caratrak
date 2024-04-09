a009434e832fb636028612fff47439ca6ce19575
Mohdhussain28 <mohdhussain28@outlook.com>
2024-04-09 16:05:12 +0530

    API: Signup with authentication

--- functions/v1/index.js
+++ functions/v1/index.js
@@ -6,11 +6,16 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 
+// Middlewares
+const authMiddleware = require("../middlewares/auth")
+
 const app = express();
 app.use(cors({ origin: true }));
 
 app.get("/", async (req, res) => {
     res.status(200).send({ message: "Hii, i am there" })
 })
+app.use(authMiddleware);
+app.use("/users", require("./routes/userRoutes"))
 
 module.exports = functions.https.onRequest(app);
