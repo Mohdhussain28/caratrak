00da264d715a255c20439eced624e559c9113ad3
Mohdhussain28 <mohdhussain28@outlook.com>
2024-04-10 17:39:45 +0530

    add swagger yaml

--- functions/v1/index.js
+++ functions/v1/index.js
@@ -1,21 +1,30 @@
-// Firebase SDK
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
-
-// Third Party Libraries
 const express = require("express");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('yamljs');
 
 // Middlewares
-const authMiddleware = require("../middlewares/auth")
+const authMiddleware = require("../middlewares/auth");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(cors({ origin: true }));
+const path = require('path');
+
+
+// Load Swagger YAML file
+const swaggerDocument = yaml.load(path.join(__dirname, './swagger.yaml'));
+
+// Serve Swagger UI at /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", async (req, res) => {
-    res.status(200).send({ message: "Hii, i am there" })
-})
+    res.status(200).send({ message: "Hii, i am there" });
+});
+
 app.use(authMiddleware);
-app.use("/users", require("./routes/userRoutes"))
+app.use("/users", userRoutes);
 
 module.exports = functions.https.onRequest(app);
