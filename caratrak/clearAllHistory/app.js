9b9d293bcd0498fb2118930d654ab7c589b6d9ff
Mohdhussain28 <mohdhussain28@outlook.com>
2024-05-07 12:09:18 +0530

    API: clear All

---
+++ caratrak/clearAllHistory/app.js
@@ -0,0 +1,35 @@
+const AWS = require("aws-sdk");
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+exports.lambdaHandler = async (event) => {
+    const id = event.queryStringParameters?.id;
+
+    if (!id) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Missing id parameter' })
+        };
+    }
+
+    const params = {
+        TableName: 'compared-table',
+        Key: {
+            id: id
+        }
+    };
+
+    try {
+        await DynamoDB.delete(params).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Item deleted successfully" })
+        };
+    } catch (error) {
+        console.error("Error deleting item:", error);
+        return {
+            statusCode: error.statusCode || 500,
+            body: JSON.stringify({ message: error.message || "Internal Server Error" })
+        };
+    }
+};
